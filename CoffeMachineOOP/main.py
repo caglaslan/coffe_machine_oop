71c10836600378f8a0498274bab403eb81f365e3
caglaslan <sevgiaslan22@hacettepe.edu.tr>
2023-08-31 00:01:08 +0300

    coffe_machine_oop
    basic coffe machine , python

---
+++ CoffeMachineOOP/main.py
@@ -0,0 +1,30 @@
+from menu import Menu, MenuItem
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
+
+menu = Menu()
+coffee_maker = CoffeeMaker()
+money_machine = MoneyMachine()
+
+
+
+is_on = True
+
+while is_on:
+    drinks = menu.get_items()
+    order = input(f"What would you like to order ({drinks})? : ").lower()
+    if order == "off":
+        is_on = False
+    elif order == "report":
+        coffee_maker.report()
+        money_machine.report()
+    else:
+        drink = menu.find_drink(order)
+        if coffee_maker.is_resource_sufficient(drink) and money_machine.make_payment(drink.cost):
+                coffee_maker.make_coffee(drink)
+
+
+
+
+
+
